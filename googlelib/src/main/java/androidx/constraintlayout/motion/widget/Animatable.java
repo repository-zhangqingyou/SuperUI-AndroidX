b726901d9512832f327779c234ba1be6a6d914e6
张风 <1334738402@qq.com>
2020-09-17 01:00:45 +0800

    **

--- googlelib/src/main/java/androidx/constraintlayout/motion/widget/Animatable.java
+++
@@ -1,9 +0,0 @@
-package androidx.constraintlayout.motion.widget;
-
-/**
- * A helper interface allowing MotionLayout to directly drive custom views
- */
-public interface Animatable {
-    void setProgress(float progress);
-    float getProgress();
-}
--- googlelib/src/main/java/androidx/core/internal/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @hide
- */
-@RestrictTo(LIBRARY_GROUP)
-package androidx.core.internal;
-
-import androidx.annotation.RestrictTo;
-
-import static androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP;

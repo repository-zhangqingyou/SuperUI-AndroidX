be99a375c7608f43ee4ca76811fca9d92d8dc0ca
张风 <1334738402@qq.com>
2020-09-20 14:17:04 +0800

    针对Android_support  * 处理R文件：  * 1.将Android_support下的所有R文件的映射地址更换  *  * 2.将本Modules生成的R文件 打包成jar

---
+++ lib_google_android_support/src/main/java/android/arch/lifecycle/R.java
@@ -0,0 +1,11 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package android.arch.lifecycle;
+
+public final class R {
+    public R() {
+    }
+}

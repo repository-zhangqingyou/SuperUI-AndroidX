274d86bd6a1b21a9e42eb5002c7dcd6838f5a14b
张风 <1334738402@qq.com>
2020-11-02 23:15:11 +0800

    app-androidx-UI库完成

---
+++ super_utils/src/main/java/com/zqy/superutils/em/ByteEnum.java
@@ -0,0 +1,30 @@
+package com.zqy.superutils.em;
+
+/**
+ * 作者: zhangqingyou
+ * 时间: 2020/11/2 23:13
+ * 描述:
+ */
+public enum ByteEnum {
+    BYTE(1),
+
+    KB(1024),
+
+    MB(1048576),
+
+    GB(1073741824);
+
+    private int v;//自定义属性
+
+    /**
+     * 构造函数，枚举类型只能为私有
+     */
+    ByteEnum(int v) {
+        this.v = v;
+    }
+
+    //自定义方法
+    public int getV() {
+        return v;
+    }
+}

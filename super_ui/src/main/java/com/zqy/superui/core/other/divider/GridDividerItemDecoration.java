c4aa0ef7be6aeb78ec1a11675c12e74edfb165a0
张风 <1334738402@qq.com>
2020-12-11 17:31:52 +0800

    View带动画显示与隐藏

--- super_ui/src/main/java/com/zqy/superui/core/other/divider/GridDividerItemDecoration.java
+++ super_ui/src/main/java/com/zqy/superui/core/other/divider/GridDividerItemDecoration.java
@@ -16,7 +16,8 @@ import androidx.recyclerview.widget.RecyclerView;
  * 描述: GridLayoutManager 专用
  */
 public class GridDividerItemDecoration extends RecyclerView.ItemDecoration {
-    private int space;
+    private int spaceRL;//指定左右间隙宽度
+    private int spaceTB;//指定上下间隙宽度
     private int color;
     private Paint mPaint;
 
@@ -30,31 +31,44 @@ public class GridDividerItemDecoration extends RecyclerView.ItemDecoration {
     /**
      * 自定义宽度的透明分割线
      *
-     * @param space 指定宽度
+     * @param space 指定左右间隙宽度
      */
     public GridDividerItemDecoration(int space) {
-        this(space, Color.TRANSPARENT);
+        this(space, space, Color.TRANSPARENT);
+    }
+
+
+    /**
+     * 自定义宽度，并指定颜色的分割线
+     *
+     * @param spaceRL 指定左右间隙宽度
+     * @param spaceTB 指定上下间隙宽度
+     */
+
+    public GridDividerItemDecoration(int spaceRL, int spaceTB) {
+        this(spaceRL, spaceTB, Color.TRANSPARENT);
     }
 
     /**
      * 自定义宽度，并指定颜色的分割线
      *
-     * @param space 指定宽度
-     * @param color 指定颜色
+     * @param spaceRL 指定左右间隙宽度
+     * @param spaceTB 指定上下间隙宽度
+     * @param color   指定颜色
      */
 
-    public GridDividerItemDecoration(int space, int color) {
-        this.space = space;
+    public GridDividerItemDecoration(int spaceRL, int spaceTB, int color) {
+        this.spaceRL = spaceRL;
+        this.spaceTB = spaceTB;
         this.color = color;
         initPaint();
     }
-
 
     private void initPaint() {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setColor(color);
         mPaint.setStyle(Paint.Style.FILL);
-        mPaint.setStrokeWidth(space);
+        mPaint.setStrokeWidth(spaceRL);
     }
 
     @Override
@@ -63,7 +77,7 @@ public class GridDividerItemDecoration extends RecyclerView.ItemDecoration {
         int childSize = parent.getChildCount();
         int span = manager.getSpanCount();
         //为了Item大小均匀，将设定分割线平均分给左右两边Item各一半
-        int offset = space / 2;
+        int offset = spaceRL / 2;
         //得到View的位置
         int childPosition = parent.getChildAdapterPosition(view);
         //第一排，顶部不画
@@ -80,11 +94,11 @@ public class GridDividerItemDecoration extends RecyclerView.ItemDecoration {
         } else {
             //上下的分割线，就从第二排开始，每个区域的顶部直接添加设定大小，不用再均分了
             if (childPosition % span == 0) {
-                outRect.set(0, space, offset, 0);
+                outRect.set(0, spaceTB, offset, 0);
             } else if (childPosition % span == span - 1) {
-                outRect.set(offset, space, 0, 0);
+                outRect.set(offset, spaceTB, 0, 0);
             } else {
-                outRect.set(offset, space, offset, 0);
+                outRect.set(offset, spaceTB, offset, 0);
             }
         }
     }

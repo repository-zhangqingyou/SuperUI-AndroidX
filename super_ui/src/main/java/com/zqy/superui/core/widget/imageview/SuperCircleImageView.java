b5ef78c7cd1169278e79ce28279687fc3cda19df
张风 <1334738402@qq.com>
2021-01-28 10:41:11 +0800

    引入 qmui库

--- super_ui/src/main/java/com/zqy/superui/core/widget/imageview/SuperCircleImageView.java
+++ super_ui/src/main/java/com/zqy/superui/core/widget/imageview/SuperCircleImageView.java
@@ -1,28 +1,28 @@
-package com.zqy.superui.core.widget.imageview;
-
-import android.content.Context;
-import android.util.AttributeSet;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatImageView;
-
-/**
- * 作者: zhangqingyou
- * 时间: 2021/1/28 9:14
- * 描述: 圆角ImageView
- */
-public class SuperCircleImageView extends AppCompatImageView {
-
-    public SuperCircleImageView(Context context) {
-        this(context, null);
-    }
-
-    public SuperCircleImageView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public SuperCircleImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-
-    }
-}
+//package com.zqy.superui.core.widget.imageview;
+//
+//import android.content.Context;
+//import android.util.AttributeSet;
+//
+//import androidx.annotation.Nullable;
+//import androidx.appcompat.widget.AppCompatImageView;
+//
+///**
+// * 作者: zhangqingyou
+// * 时间: 2021/1/28 9:14
+// * 描述: 圆角ImageView
+// */
+//public class SuperCircleImageView extends AppCompatImageView {
+//
+//    public SuperCircleImageView(Context context) {
+//        this(context, null);
+//    }
+//
+//    public SuperCircleImageView(Context context, @Nullable AttributeSet attrs) {
+//        this(context, attrs, 0);
+//    }
+//
+//    public SuperCircleImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//
+//    }
+//}

e79a94cfce7864f9e8bcf0a092eb2e72f052e541
张风 <1334738402@qq.com>
2021-06-02 18:15:32 +0800

    更新Bugly

---
+++ super_utils/src/main/java/com/zqy/superutils/provider/BuglyFileProvider.java
@@ -0,0 +1,11 @@
+package com.zqy.superutils.provider;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * 作者: zhangqingyou
+ * 时间: 2021/6/2
+ * 描述:
+ */
+public class BuglyFileProvider extends FileProvider {
+}

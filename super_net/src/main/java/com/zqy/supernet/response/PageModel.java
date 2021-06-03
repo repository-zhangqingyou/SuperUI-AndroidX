6e23d9fdfc6fabf4af1acc88821bd70dbecfb67b
张风 <1334738402@qq.com>
2021-06-03 11:35:15 +0800

    更新Bugly

---
+++ super_net/src/main/java/com/zqy/supernet/response/PageModel.java
@@ -0,0 +1,28 @@
+package com.zqy.supernet.response;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 作者: zhangqingyou
+ * 时间: 2021/6/3 11:05
+ * 描述:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@ApiModel("翻页返回数据Model")
+public class PageModel<T> implements Serializable {
+
+   // @ApiModelProperty(value = "总数", example = "99999")
+    private Long total;
+    //@ApiModelProperty(value = "数据")
+    private T data;
+
+    public static <D> PageModel<D> getPageModel(Long total, D data) {
+        return new PageModel<D>(total, data);
+    }
+}

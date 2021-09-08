cf1c31faed405c339b36d6685c30a3192c374fe0
张风 <1334738402@qq.com>
2021-09-08 15:07:22 +0800

    添加easyrecyclerview

---
+++ easyrecyclerview/src/main/java/com/zqy/easyrecyclerview/adapter/BH.java
@@ -0,0 +1,11 @@
+package com.zqy.easyrecyclerview.adapter;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class BH extends RecyclerView.ViewHolder {
+    public BH(View itemView) {
+        super(itemView);
+    }
+}

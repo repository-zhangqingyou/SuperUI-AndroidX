10a382382e998d7a9df07f24b83a926a9427bd6f
张风 <1334738402@qq.com>
2021-11-23 13:39:14 +0800

    修复bug

--- app/src/main/java/com/zqy/suidemo/activity/MainActivity.java
+++ app/src/main/java/com/zqy/suidemo/activity/MainActivity.java
@@ -20,12 +20,12 @@ import com.blankj.utilcode.util.ResourceUtils;
 import com.blankj.utilcode.util.SizeUtils;
 import com.google.gson.Gson;
 import com.qmuiteam.qmui.widget.QMUIRadiusImageView2;
-import com.xuexiang.xui.widget.imageview.preview.PreviewBuilder;
 import com.zqy.suidemo.R;
 import com.zqy.suidemo.model.AppInfo;
 import com.zqy.superui.core.enums.Gradient;
 import com.zqy.superui.core.module.ImageViewInfo;
 import com.zqy.superui.core.other.drawable.SuperStateListDrawable;
+import com.zqy.superui.core.ui.activity.preview.SuperUISinglePreviewActivity;
 import com.zqy.superui.core.widget.framelayout.SuperFrameLayout;
 import com.zqy.superui.core.widget.popup.tips.LoadPopup;
 import com.zqy.superui.core.widget.textview.SuperTextView;
@@ -176,11 +176,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.iv_preview://图片预览
                 String img = "https://image.baidu.com/search/down?tn=download&word=download&ie=utf8&fr=detail&url=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%253A%252F%252Fimg.jj20.com%252Fup%252Fallimg%252Ftp05%252F1910020U602A43-0-lp.jpg%26refer%3Dhttp%253A%252F%252Fimg.jj20.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1640223918%26t%3Dd80c4abeb270fc8999d3428781c82049&thumburl=https%3A%2F%2Fimg0.baidu.com%2Fit%2Fu%3D638855217%2C3271693300%26fm%3D26%26fmt%3Dauto";
                 ImageViewInfo imageViewInfo = new ImageViewInfo(img, mIvPreview);
-//                intent = new Intent(this, SuperUISinglePreviewActivity.class);
-//                intent.putExtra(SuperUISinglePreviewActivity.IMAGE_VIEW_INFO_KEY, imageViewInfo);
-//                startActivity(intent);
+                intent = new Intent(this, SuperUISinglePreviewActivity.class);
+                intent.putExtra(SuperUISinglePreviewActivity.TITLE_KEY, "单图预览");
+                intent.putExtra(SuperUISinglePreviewActivity.IMAGE_VIEW_INFO_KEY, imageViewInfo);
+                startActivity(intent);
 
-               PreviewBuilder.from(this).setImg(imageViewInfo).start();
+              // PreviewBuilder.from(this).setImg(imageViewInfo).start();
+//                PreviewBuilder.from(this)
+//                        .setImg(imageViewInfo)
+//                        .setSingleFling(true)
+//                        .setType(PreviewBuilder.IndicatorType.Number)
+//                        .start();
                 break;
         }
     }

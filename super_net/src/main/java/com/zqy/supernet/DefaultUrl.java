d6ecace5111d3f161d406b8a08e8380abb36813c
张风 <1334738402@qq.com>
2021-12-22 21:03:51 +0800

    **

---
+++ super_net/src/main/java/com/zqy/supernet/DefaultUrl.java
@@ -0,0 +1,13 @@
+package com.zqy.supernet;
+
+/**
+ * 作者: zhangqingyou
+ * 时间: 2021/12/22
+ * 描述: 我的默认URL
+ */
+public class DefaultUrl {
+    public static final String INTERNET_DESKTOP = "http://family.zhangqingyou.top";//家里-公网-台式机
+    public static final String INTERNET_NOTEBOOK = "http://notebook.zhangqingyou.top";//家里-公网-笔记本
+    public static final String LOCALHOST_DESKTOP = "http://localhost.desktop.zhangqingyou.top";//家里-内网-台式机ip
+    public static final String LOCALHOST_NOTEBOOK = "http://localhost.notebook.zhangqingyou.top";//家里-内网-笔记本ip
+}
